fbc61375b4b536ab8d86649d6204484de6af6ae4
wangning1996chris <wangning1996chris@gmail.com>
2024-02-19 10:31:14 +0800

    Simplify Code

--- PPOPlanner/MapData/MapGen.py
+++ PPOPlanner/MapData/MapGen.py
@@ -5,91 +5,51 @@ import csv
 import sys
 import os
 
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
-#                 "/../../MotionPlanning/")
+
+## Map Configuration
+XScale = 2
+YScale = 2
 current_directory = os.path.dirname(os.path.realpath(__file__))
+
 
 def read_csv_file(file_path):
     coordinates_list = []
-
     with open(file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        
-        # Skip the header
         next(csv_reader)
-
         for row in csv_reader:
-            # Convert string values to float and create a tuple
             x, y = map(float, row[0:2])
-            coordinates_list.append((x, y))
-
+            coordinates_list.append((x * XScale, y * YScale))
     return coordinates_list
 
-def plot_rectangles(coordinates):
-    fig, ax = plt.subplots()
 
-    for (x, y) in coordinates:
-        # Create a rectangle centered at (x, y) with width=4 and height=2
-        rect = Rectangle((x - 2, y - 1), 4, 2, linewidth=1, edgecolor='black', facecolor='yellow')
-        ax.add_patch(rect)
-
-def design_24rectangles(points):
+def design_rectangles(points, x_offset, y_offset):
     connected_points = []
-
     for (x, y) in points:
-
-        # Append the four points
-        connected_points.append((x - 1, y + 2))
-        connected_points.append((x - 1, y - 2))
-        connected_points.append((x + 1, y - 2))
-        connected_points.append((x + 1, y + 2))
-        connected_points.append((x - 1, y + 2))
-    
-    # x_values, y_values = zip(*connected_points)
-
-    # return x_values, y_values
+        connected_points.append((x - x_offset, y + y_offset))
+        connected_points.append((x - x_offset, y - y_offset))
+        connected_points.append((x + x_offset, y - y_offset))
+        connected_points.append((x + x_offset, y + y_offset))
+        connected_points.append((x - x_offset, y + y_offset))
     return connected_points
 
-def design_42rectangles(points):
-    connected_points = []
-
-    for (x, y) in points:
-
-        # Append the four points
-        connected_points.append((x - 2, y + 1))
-        connected_points.append((x - 2, y - 1))
-        connected_points.append((x + 2, y - 1))
-        connected_points.append((x + 2, y + 1))
-        connected_points.append((x - 2, y + 1))
-    
-    # x_values, y_values = zip(*connected_points)
-
-    # return x_values, y_values
-    return connected_points
 
 def draw_map(obstacle_points, truck_points, parkingspot_points):
     # draw outlines
-    x_values = [-35, 35, 35, -35, -35]
-    y_values = [-55, -55, 55, 55, -55]
-    plt.plot(x_values, y_values, color='black', linewidth=2)
+    x_outlines = [-35, 35, 35, -35, -35]
+    y_outlines = [-55, -55, 55, 55, -55]
+    x_outlines_scaled = list(map(lambda x : x * XScale, x_outlines))
+    y_outlines_scaled = list(map(lambda y : y * YScale, y_outlines))
+    plt.plot(x_outlines_scaled, y_outlines_scaled, color='black', linewidth=2)
 
-    # draw obstacles
-    for i in range(0, len(obstacle_points), 5):
-        x_values, y_values = zip(*obstacle_points[i:i+5])
-        plt.plot(x_values, y_values, color='blue', linestyle='-')
+    # draw obstacles, trucks, and parking spots
+    for points, color in zip([obstacle_points, truck_points, parkingspot_points], ['blue', 'red', 'yellow']):
+        for i in range(0, len(points), 5):
+            x_values, y_values = zip(*points[i:i+5])
+            plt.plot(x_values, y_values, color=color, linestyle='-')
 
-    # draw trucks
-    for i in range(0, len(truck_points), 5):
-        x_values, y_values = zip(*truck_points[i:i+5])
-        plt.plot(x_values, y_values, color='red', linestyle='-')
-        
-    # draw parking spot
-    for i in range(0, len(parkingspot_points), 5):
-        x_values, y_values = zip(*parkingspot_points[i:i+5])
-        plt.plot(x_values, y_values, color='yellow', linestyle='-')
 
 def main():
-
     obstacle_file_path = os.path.join(current_directory, 'csvfiles', 'Obstaclecsv', 'obstacle_coordinates.csv')
     truck_file_path = os.path.join(current_directory, 'csvfiles', 'Truckcsv', 'truck_coordinates.csv')
     parkingspot_path = os.path.join(current_directory, 'csvfiles', 'Goalcsv', 'goal_coordinates.csv')
@@ -97,9 +57,9 @@ def main():
     TruckCoordinates = read_csv_file(truck_file_path)
     ParkingSpotCoordinates = read_csv_file(parkingspot_path)
 
-    obstacle_points = design_24rectangles(ObstacleCoordinates)
-    truck_points = design_42rectangles(TruckCoordinates)
-    parkingspot_points = design_42rectangles(ParkingSpotCoordinates)
+    obstacle_points = design_rectangles(ObstacleCoordinates, 1 * XScale, 2 * YScale)
+    truck_points = design_rectangles(TruckCoordinates, 2 * XScale, 1 * YScale)
+    parkingspot_points = design_rectangles(ParkingSpotCoordinates, 2 * XScale, 1 * YScale)
 
     draw_map(obstacle_points, truck_points, parkingspot_points)
 
--- PPOPlanner/TrajectoryTrack/Pure_Pursuit.py
+++ PPOPlanner/TrajectoryTrack/Pure_Pursuit.py
@@ -1,6 +1,7 @@
 """
-Pure Pursuit
-author: huiming zhou
+Pure Pursuit For RL
+authors: Wang Ning; Yang Hongyi
+Date: 2024-02-01
 """
 
 import os
@@ -8,6 +9,8 @@ import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import Utils.reeds_shepp as rs
+from pid_config import C
 
 
 class Nodes:
@@ -26,3 +29,78 @@ class PATH:
         self.cy = cy
         self.ind_end = len(self.cx) - 1
         self.index_old = None
+
+
+def generate_path(s):
+    """
+    divide paths into some sections, in each section, the direction is the same.
+    :param s: target position and yaw
+    :return: sections
+    """
+
+    max_c = math.tan(C.MAX_STEER) / C.WB  # max curvature
+
+    path_x, path_y, yaw, direct = [], [], [], []
+    x_rec, y_rec, yaw_rec, direct_rec = [], [], [], []
+    direct_flag = 1.0
+
+    for i in range(len(s) - 1):
+        s_x, s_y, s_yaw = s[i][0], s[i][1], np.deg2rad(s[i][2])
+        g_x, g_y, g_yaw = s[i + 1][0], s[i + 1][1], np.deg2rad(s[i + 1][2])
+
+        path_i = rs.calc_optimal_path(s_x, s_y, s_yaw,
+                                      g_x, g_y, g_yaw, max_c)
+
+        ix = path_i.x
+        iy = path_i.y
+        iyaw = path_i.yaw
+        idirect = path_i.directions
+
+        for j in range(len(ix)):
+            if idirect[j] == direct_flag:
+                x_rec.append(ix[j])
+                y_rec.append(iy[j])
+                yaw_rec.append(iyaw[j])
+                direct_rec.append(idirect[j])
+            else:
+                if len(x_rec) == 0 or direct_rec[0] != direct_flag:
+                    direct_flag = idirect[j]
+                    continue
+
+                path_x.append(x_rec)
+                path_y.append(y_rec)
+                yaw.append(yaw_rec)
+                direct.append(direct_rec)
+                x_rec, y_rec, yaw_rec, direct_rec = \
+                    [x_rec[-1]], [y_rec[-1]], [yaw_rec[-1]], [-direct_rec[-1]]
+
+    path_x.append(x_rec)
+    path_y.append(y_rec)
+    yaw.append(yaw_rec)
+    direct.append(direct_rec)
+
+    x_all, y_all = [], []
+
+    for ix, iy in zip(path_x, path_y):
+        x_all += ix
+        y_all += iy
+
+    return path_x, path_y, yaw, direct, x_all, y_all
+
+
+def main():
+    # generate path: [x, y, yaw]
+    states = [(0, 0, 0), (20, 15, 0), (35, 20, 90), (40, 0, 180),
+              (20, 0, 120), (5, -10, 180), (15, 5, 30)]
+    x, y, yaw, direct, path_x, path_y = generate_path(states)
+
+    # simulation
+    maxTime = 100.0
+    yaw_old = 0.0
+    x0, y0, yaw0, direct0 = x[0][0], y[0][0], yaw[0][0], direct[0][0]
+    x_rec, y_rec = [], []
+
+
+
+if __name__ == '__main__':
+    main()
