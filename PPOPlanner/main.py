e80a67b4a9c6df6bb6d9af98e10df04beceab6e8
Violet-B <billy15928998410@outlook.com>
2024-03-11 16:26:57 +0800

    new file:   PPOPlanner/CarConfig/__init__.py 	new file:   PPOPlanner/CarConfig/__pycache__/__init__.cpython-38.pyc 	new file:   PPOPlanner/CarConfig/__pycache__/pid_config.cpython-38.pyc 	modified:   PPOPlanner/CarConfig/pid_config.py 	modified:   PPOPlanner/MapData/MapGen.py 	new file:   PPOPlanner/MapData/__pycache__/MapGen.cpython-38.pyc 	modified:   PPOPlanner/MapData/csvfiles/Goalcsv/goal_coordinates.csv 	modified:   PPOPlanner/MapData/csvfiles/Obstaclecsv/obstacle_coordinates.csv 	modified:   PPOPlanner/MapData/csvfiles/Pathcsv/path_coordinates.csv 	new file:   PPOPlanner/Sim2Real/__init__.py 	new file:   PPOPlanner/Sim2Real/__pycache__/__init__.cpython-38.pyc 	new file:   PPOPlanner/Sim2Real/__pycache__/path_optimize.cpython-38.pyc 	modified:   PPOPlanner/Sim2Real/path_optimize.py 	new file:   PPOPlanner/TrajectoryTrack/__pycache__/path_smooth.cpython-38.pyc 	new file:   PPOPlanner/TrajectoryTrack/__pycache__/reeds_shepp.cpython-38.pyc 	modified:   PPOPlanner/main.py 	new file:   UnmannedOperating/ControlAlgorithm/__pycache__/LQR_Kinematic_Model.cpython-38.pyc 	new file:   UnmannedOperating/ControlAlgorithm/__pycache__/utils.cpython-38.pyc 	new file:   UnmannedOperating/PlanningAlgorithm/__pycache__/astar.cpython-38.pyc 	new file:   UnmannedOperating/PlanningAlgorithm/__pycache__/reeds_shepp.cpython-38.pyc 	new file:   UnmannedOperating/__pycache__/TrailerCar.cpython-38.pyc 	new file:   UnmannedOperating/__pycache__/config.cpython-38.pyc 	new file:   UnmannedOperating/__pycache__/enviroment.cpython-38.pyc 	new file:   UnmannedOperating/__pycache__/planner.cpython-38.pyc 	new file:   UnmannedOperating/__pycache__/units.cpython-38.pyc 	modified:   UnmannedOperating/data/trailer_path_2.pkl 	modified:   UnmannedOperating/run.py 	modified:   outputcsvfiles/opimaized_path.csv

--- PPOPlanner/main.py
+++ PPOPlanner/main.py
@@ -1,19 +1,27 @@
 from CarConfig.pid_config import C
-from MapData.MapGen import plot_map, get_rl_path
+from MapData.MapGen import plot_map, get_rl_path, get_rl_goal
 from TrajectoryTrack.path_smooth import generate_path
+from Sim2Real.path_optimize import get_sample_points
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Init map infomation
     obs_points, truck_points, park_points = plot_map(show_flag=False)
     rl_states = get_rl_path()
-    states_downsample = rl_states[::C.sample_rate]
+    x, y, angle = zip(*rl_states)
+    goalx, goaly = get_rl_goal()
+    x_turning, y_turning, angle_turning, point_number = get_sample_points(x, y, angle, goalx, goaly)
+    states_downsample = [[a, b, c] for a, b, c in zip(x_turning, y_turning, angle_turning)]
+    # states_downsample = rl_states[::C.sample_rate]
+    print(states_downsample)
 
     # Paht Smoothing
     x, y, yaw, direct, path_x, path_y = generate_path(states_downsample)
+    
 
 
     # animation
     plt.cla()
     plt.plot(path_x, path_y, color='gray', linewidth=2)
+    plt.axis('equal')
     plt.show()
